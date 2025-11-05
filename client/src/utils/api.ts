bb073acfb9f59075f44231d4300aeb6e432a0de2
ali-imtiyazkhan <itzkhantijara@gmail.com>
2025-11-06 01:36:32 +0530

    type error

--- client/src/utils/api.ts
+++ client/src/utils/api.ts
@@ -1,4 +1,4 @@
-export const API_BASE_URL = "http://localhost:3001";
+export const API_BASE_URL = "https://e-commerceweb-2-dy4t.onrender.com";
 
 export const API_ROUTES = {
   AUTH: `${API_BASE_URL}/api/auth`,
